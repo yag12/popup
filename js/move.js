b77106fd1893fa9aa2fe20bc4e175de3a146ea76
yag12 <yag12@lycos.co.kr>
2014-07-26 14:54:03 +0900

    keydown 이벤트 발생시 예외처리

--- js/move.js
+++ js/move.js
@@ -33,29 +33,32 @@
 		$(document).keydown(function(e){
 			var key = e.keyCode;
 
-			if(key == 27) // Esc
+			if(typeof moveElementFocus != "undefined")
 			{
-				delete moveElementFocus;
-			}
-
-			if(moveElementFocus == moveElement)
-			{
-				if(moveElement.css("left") != "auto")
+				if(key == 27) // Esc
 				{
-					var m = 10;
-					switch(key){
-						case 38: // ↑
-							moveElement.css("top", "-=" + m);
-							break;
-						case 40: // ↓
-							moveElement.css("top", "+=" + m);
-							break;
-						case 39: // →
-							moveElement.css("left", "+=" + m);
-							break;
-						case 37: // ←
-							moveElement.css("left", "-=" + m);
-							break;
+					delete moveElementFocus;
+				}
+
+				if(moveElementFocus == moveElement)
+				{
+					if(moveElement.css("left") != "auto")
+					{
+						var m = 10;
+						switch(key){
+							case 38: // ↑
+								moveElement.css("top", "-=" + m);
+								break;
+							case 40: // ↓
+								moveElement.css("top", "+=" + m);
+								break;
+							case 39: // →
+								moveElement.css("left", "+=" + m);
+								break;
+							case 37: // ←
+								moveElement.css("left", "-=" + m);
+								break;
+						}
 					}
 				}
 			}
